1caf00e553733135ae7e84dbd4bd6227784aab72
sudeqiangxx <1320909689@qq.com>
2017-11-17 14:26:43 +0800

    视频

---
+++ app/src/main/java/cn/com/sdq/smilefriends/interf/BackHadleInterface.java
@@ -0,0 +1,13 @@
+package cn.com.sdq.smilefriends.interf;
+
+import cn.com.sdq.smilefriends.base.BaseTwoFragment;
+
+/**
+ * Created by sudeqiang on 2017/11/17.
+ * 邮箱：1320909689@qq.com
+ * 博客地址：https://sudeqiangxx.github.io
+ */
+
+public interface BackHadleInterface {
+    public abstract void setSelectedFragment(BaseTwoFragment selectedFragment);
+}
